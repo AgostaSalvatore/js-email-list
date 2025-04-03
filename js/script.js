f39e8c8f91ae67b835578bca1329211f95d9f4ea
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-03 14:21:06 +0200

    [DONE] all email are displayed on page

--- js/script.js
+++ js/script.js
@@ -3,13 +3,18 @@ const emailList = document.getElementById("email-list");
 
 //mi dichiaro le variabili
 const mails = [];
-let items = 10;
-const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail`;
+let items = 100;
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
 
 for (let i = 0; i < items; i++) {
     axios.get(endpoint).then((response) => {
         const mail = response.data.response;
         console.log(mail);
+        
+        // Aggiungo l'email all'array
+        mails.push(mail);
 
+        // Aggiungo l'email alla lista HTML
+        emailList.innerHTML += `<li class="list-group-item">${mail}</li>`;
     });
 }
