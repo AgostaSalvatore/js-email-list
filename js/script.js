82c70201f258e41643fcefd4c180aba158aaecc9
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-03 14:28:45 +0200

    [DONE] bonus with button using axios

--- js/script.js
+++ js/script.js
@@ -1,20 +1,31 @@
 //recupero gli elementi del dom
 const emailList = document.getElementById("email-list");
+const button = document.getElementById("listAgain");
 
 //mi dichiaro le variabili
 const mails = [];
-let items = 100;
+let items = 10;
 const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?count=${items}`;
 
-for (let i = 0; i < items; i++) {
-    axios.get(endpoint).then((response) => {
-        const mail = response.data.response;
-        console.log(mail);
-        
-        // Aggiungo l'email all'array
-        mails.push(mail);
+//funzione evento bottone
 
-        // Aggiungo l'email alla lista HTML
-        emailList.innerHTML += `<li class="list-group-item">${mail}</li>`;
-    });
+const recreateList = () => {
+    // Resetto l'array
+    emailList.innerHTML = '';
+    for (let i = 0; i < items; i++) {
+        axios.get(endpoint).then((response) => {
+            const mail = response.data.response;
+            console.log(mail);
+            
+            // Aggiungo l'email all'array
+            mails.push(mail);
+    
+            // Aggiungo l'email alla lista HTML
+            emailList.innerHTML += `<li class="list-group-item">${mail}</li>`;
+        });
+    }
+    console.clear();
 }
+
+button.addEventListener("click", recreateList);
+recreateList();
