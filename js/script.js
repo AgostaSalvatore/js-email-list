dabf5624cf9824a501cc0a24bacc9e49bdaae4e1
AgostaSalvatore <agosta.boolean@gmail.com>
2025-04-03 13:54:43 +0200

    [UPD] created variables

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
 //recupero gli elementi del dom
 const emailList = document.getElementById("email-list");
-const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+
+//mi dichiaro le variabili
+let items = 10;
+const endpoint = `https://flynn.boolean.careers/exercises/api/random/mail?items=${items}`;
+
